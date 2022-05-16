b34b6b94661badd79e05da2d6bdb1aff17a1d84e
Catherine_Vuthi <vuthi.esther@gmail.com>
2022-05-16 18:54:07 +0300

    completed assignment

---
+++ src/context/user.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// create the context
+const UserContext = React.createContext();
+
+// create a provider component
+function UserProvider({ children }) {
+    const [user, setUser] = useState(null);
+  // the value prop of the provider will be our context data
+  // this value will be available to child components of this provider
+  return <UserContext.Provider value={null}>{children}</UserContext.Provider>;
+}
+
+export { UserContext, UserProvider };
